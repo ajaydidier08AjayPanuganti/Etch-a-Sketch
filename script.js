d3a7d812b6fc695f886783ff77da7364a79525ef
Ajay <ajaypan1998@gmail.com>
2023-11-20 22:25:22 -0800

    Fixed resetting of grid

--- script.js
+++ script.js
@@ -2,19 +2,27 @@ var creategrid = document.querySelector(".click")
 var grid = document.querySelector(".grid")
 
 creategrid.addEventListener("click", ()=>{ 
-var gridsize = document.querySelector("input")
-for(let i = 0;i < gridsize;i++){
-    for(let j = 0;j < gridsize;j++){
-        var box = document.createElement("div")
-        box.classList.add("box")        
-        box.style.width = grid.clientWidth/gridsize - 4 + "px"
-        box.style.height = grid.clientHeight/gridsize - 4 + "px"
-        grid.appendChild(box)
-
+var gridsize = document.querySelector("input").value
+    reset()
+    for(let i = 0;i < gridsize;i++){
+        for(let j = 0;j < gridsize;j++){
+            var box = document.createElement("div")
+            box.classList.add("box")        
+            box.style.width = grid.clientWidth/gridsize - 2 + "px"
+            box.style.height = grid.clientHeight/gridsize - 2 + "px"
+            grid.appendChild(box)
+        }
     }
-}
 })
 
+function reset(){
+    let count = 0
+    while(grid.firstChild){
+        grid.removeChild(grid.firstChild)
+        count++
+    }
+    console.log(count)
+}
 /*function createBox(){
     var box = document.createElement("div")
     box.classList.add("box")
