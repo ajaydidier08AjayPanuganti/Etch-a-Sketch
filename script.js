ee06b2608492f70b233387601f4163706c81f676
Ajay <ajaypan1998@gmail.com>
2023-11-23 22:09:49 -0800

    Cosmetic changes final commit

--- script.js
+++ script.js
@@ -29,38 +29,3 @@ grid.addEventListener("mouseover",function(e){
         e.target.classList.add("clicked")
     }
 })
-/*function createBox(){
-    var box = document.createElement("div")
-    box.classList.add("box")
-    box.style.height = "50px"
-    box.style.widht = "50px"
-    box.style.backgroundColor = "white"
-    grid.appendChild(box)
-    
-    var clickcount = 1
-    
-    //document.querySelector("body").innerHTML = clickcount + 1
-}
-
-/*var gridsize = 4
-var submit  = document.querySelector(".submit")
-var grid = document.querySelector(".grid")
-gridsize = document.querySelector("input") 
-grid.addEventListener('click',createGrid(gridsize))
-
-
-
-function createGrid(gridsize){
-    for(let i = 0;i<gridsize;i++){
-        for(let j = 0; j<gridsize;j++){
-            createBox(grid.clientWidth/gridsize)
-        }
-    }
-}
-function createBox(dime){
-    var box = document.createElement("div")
-    box.classList.add("box")
-    box.style.width = dime
-    box.style.heigh = dime
-    grid.appendChild(box)
-}*/
